cecc1d36d0e2e2f646a94d345df94f050b3741ce
nnsnodnb <nnsnodnb@gmail.com>
2024-12-04 00:53:07 +0900

    Update background

--- ChibaSwiftSwiftLinuxSDKPresentation/Slides/HelloFuller.swift
+++ ChibaSwiftSwiftLinuxSDKPresentation/Slides/HelloFuller.swift
@@ -13,10 +13,12 @@ struct HelloFuller: View {
   var body: some View {
     VStack(alignment: .center, spacing: 20) {
       Text("フラーの皆さんお久しぶりです👋")
-        .font(.system(size: 120, weight: .black))
+        .font(.system(size: 120, weight: .bold))
       Text("Long time no see, everyone at Fuller.")
         .font(.system(size: 60))
     }
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background(Color(.fuller))
   }
 }
 
