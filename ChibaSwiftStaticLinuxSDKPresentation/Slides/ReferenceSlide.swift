9e5195c12b235869ffb3ff2ab13e69f88be5abdb
nnsnodnb <nnsnodnb@gmail.com>
2024-12-04 19:21:06 +0900

    Format

--- ChibaSwiftStaticLinuxSDKPresentation/Slides/ReferenceSlide.swift
+++ ChibaSwiftStaticLinuxSDKPresentation/Slides/ReferenceSlide.swift
@@ -10,27 +10,27 @@ import SwiftUI
 
 @Slide
 struct ReferenceSlide: View {
-    var body: some View {
-      HeaderSlide(
-        header: {
-          SlideTitle(japanese: "参考", english: "References")
-        },
-        content: {
-          Item("Swiftの新機能", accessory: 1) {
-            Item(
-              "https://developer.apple.com/jp/videos/play/wwdc2024/10136/",
-              accessory: nil
-            )
-          }
-          Item("Getting Started with the Static Linux SDK", accessory: 2) {
-            Item(
-              "https://www.swift.org/documentation/articles/static-linux-getting-started.html",
-              accessory: nil
-            )
-          }
+  var body: some View {
+    HeaderSlide(
+      header: {
+        SlideTitle(japanese: "参考", english: "References")
+      },
+      content: {
+        Item("Swiftの新機能", accessory: 1) {
+          Item(
+            "https://developer.apple.com/jp/videos/play/wwdc2024/10136/",
+            accessory: nil
+          )
         }
-      )
-    }
+        Item("Getting Started with the Static Linux SDK", accessory: 2) {
+          Item(
+            "https://www.swift.org/documentation/articles/static-linux-getting-started.html",
+            accessory: nil
+          )
+        }
+      }
+    )
+  }
 }
 
 #Preview {
